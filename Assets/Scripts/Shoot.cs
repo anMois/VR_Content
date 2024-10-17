c67eec6b249a3281023d0c8485577b2cecdfa628
anMois <kew1672@naver.com>
2024-10-17 17:34:30 +0900

    [Feat] 컨트롤러로 총 조작
    Activate 이벤트로 총알 오브젝트 생성과 레이 캐스트로 인한 오브젝트 확인 구현

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,11 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    public void Fire()
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform muzzlePoint;
+    [SerializeField] float speed;
+
+    public void Fire_GameObject()
     {
-        Debug.Log("¹ßÆ÷!");
+        GameObject bullet = Instantiate(bulletPrefab, muzzlePoint.position, muzzlePoint.rotation);
+        Rigidbody rigid = bullet.GetComponent<Rigidbody>();
+        rigid.velocity = bullet.transform.forward * speed;
+    }
+
+    public void Fire_Raycast()
+    {
+        if(Physics.Raycast(muzzlePoint.position, muzzlePoint.forward, out RaycastHit hit, 100))
+        {
+            Renderer render = hit.collider.gameObject.GetComponent<Renderer>();
+            if(hit.collider.name.Equals("Cube"))
+            {
+                render.material.color = Color.yellow;
+            }
+        }
     }
 }
